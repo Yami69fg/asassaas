1664319a45fc7f6c0b803ca859888747eafa0baf
Yami69fg <135854840+Yami69fg@users.noreply.github.com>
2024-10-17 14:46:15 +0300

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UINavigationController(rootViewController: LoadingController())
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
